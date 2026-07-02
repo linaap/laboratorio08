bcf411fb5b4e5a55c406d587cd087f268ee726dd
seven7739 <josuebalarezo0@gmail.com>
2026-07-01 19:56:11 -0500

    a;adi las funciones del ejercicio1

--- laboratorio8.h
+++ laboratorio8.h
@@ -7,4 +7,12 @@ int stringAInt(std::string texto);
 void revisarSiglas(char texto[]);
 char identificarCharMasRepetido(char texto[]);
 
+//funciones ejericicio1
+double calcularMedia(double arreglo[], int n);
+double calcularVarianza(double arreglo[], int n);
+double calcularDesviacion_estandar(double arreglo[], int n);
+double calcularCurtosis(double arreglo[], int n);
+double encontrarMaximo(double arreglo[], int n);
+double encontrarMinimo(double arreglo[], int n);
+
 #endif
